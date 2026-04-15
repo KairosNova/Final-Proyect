eb22a27d3d6e85685a5c7ac37051cbaa909258b3
KairosNova <jhonalexreyescris@gmail.com>
2026-04-15 15:32:04 -0500

    Flip fixed
    The flip on the enemy is fixed and the base logic on the big enemy is created

--- Assets/Scripts/BigGuyEnemy.cs
+++ Assets/Scripts/BigGuyEnemy.cs
@@ -2,15 +2,30 @@ using UnityEngine;
 
 public class BigGuyEnemy : EnemyBase
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [Header("Big Guy")]
+    [SerializeField] private int damage = 50;
+    private Health playerHealth;
+    protected override void Awake()
     {
-        
+        base.Awake();
+        health.Initialize(400);
+        playerHealth = player.GetComponent<Health>();
+    }
+    protected override void ChasePlayer()
+    {
+        base.ChasePlayer();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            playerHealth.TakeDamage(damage);
+            
+        }
+    }
+    public override void OnDeath()
+    {
+        Destroy(gameObject);
     }
 }
