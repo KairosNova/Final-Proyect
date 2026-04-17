30eda74a9fc08ed6f31893260f63b210e8183643
Alahros <alejoahumada94@gmail.com>
2026-04-17 10:55:34 -0500

    BigGuy All animations, Dog movement animations
    BigGuy_Attack1
    BigGuy_Attack2
    BigGuy_Idle
    BigGuy_Death
    BigGuy_Walking
    BigGuy_Weakened
    
    Dog_Walking
    Dog_Running
    
    Pivot adjusted on each animation

---
+++ Assets/Scripts/AlahrosScripts/EnemyDetection.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class EnemyDetection : MonoBehaviour
+{
+    public Transform player;
+    public float detectionRange = 10f;
+
+    void Update()
+    {
+        float distance = Vector2.Distance(transform.position, player.position);
+
+        if (distance < detectionRange)
+        {
+            Debug.Log("Player detected!");
+            // 👉 Call attack or chase here
+        }
+    }
+}
