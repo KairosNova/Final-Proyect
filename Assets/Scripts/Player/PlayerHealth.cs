f2ef93b506ea8626791df82d488d4cb2cf57e1c5
KairosNova <jhonalexreyescris@gmail.com>
2026-04-23 13:57:41 -0500

    Scriptable
    se crea scriptable de la vida

--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -3,11 +3,29 @@ using UnityEngine;
 
 public class PlayerHealth : Health
 {
+    [Header("Datos Persistentes")]
+    public PlayerData playerData;
     public static event Action<float> onHealthPercentageChanged;
+    public void Start()
+    {
+        if (playerData != null)
+        {
+            this.maxHealth = playerData.maxHealth;
+            this.currentHealth = playerData.currentHealth;
+            // Actualizamos la UI inmediatamente al cargar la escena
+            onHealthPercentageChanged?.Invoke(this.currentHealth / this.maxHealth);
+        }
+        
+    }
 
     public override void TakeDamage(int damage)
     {
         base.TakeDamage(damage);
+        if (playerData != null)
+        {
+            playerData.currentHealth = this.currentHealth;
+        }
+        
         onHealthPercentageChanged?.Invoke(base.CurrentHealth / base.MaxHealth);
     }
 }
