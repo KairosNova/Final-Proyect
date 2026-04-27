4bf706e0c5980701719c48477f7649de37c6fc7f
Alahros <alejoahumada94@gmail.com>
2026-04-27 14:14:24 -0500

    Main Menu

---
+++ Assets/Scripts/AlahrosScripts/MainMenuManager.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour
+{
+    [Header("Panels")]
+    public GameObject mainPanel;
+    public GameObject settingsPanel;
+
+    [Header("Audio")]
+    public AudioSource musicSource;
+
+    void Start()
+    {
+        if (!musicSource.isPlaying)
+        {
+            musicSource.Play();
+        }
+    }
+    // 🎮 START GAME
+    public void StartGame()
+    {
+        Debug.Log("Starting Game...");
+        SceneManager.LoadScene("UpperShipTest"); 
+    }
+
+    // ⚙️ OPEN SETTINGS
+    public void OpenSettings()
+    {
+        mainPanel.SetActive(false);
+        settingsPanel.SetActive(true);
+    }
+
+    // 🔙 BACK TO MENU
+    public void BackToMenu()
+    {
+        settingsPanel.SetActive(false);
+        mainPanel.SetActive(true);
+    }
+}
